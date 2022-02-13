18e3154ece5356a7652b4fe6229b4f5f7f8ddbc9
O-Jun-S <jun.shima256@gmail.com>
2022-02-13 17:11:57 +0900

    暗号文を入力するフォームを作成

--- src/index.js
+++ src/index.js
@@ -2,7 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Input extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  render() {
+    return (
+      <form>
+        <label>
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+            placeholder="encrypted string"
+          />
+        </label>
+      </form>
+    )
+  }
+}
+
+
 ReactDOM.render(
-  <h1>Hello World!</h1>,
+  <Input/>,
   document.getElementById('root')
 );
