d801e4e54d861210796c6a8f574a481b0b9790e6
O-Jun-S <jun.shima256@gmail.com>
2022-02-13 18:13:48 +0900

    InputのstateをPagesにリフトアップ

--- src/index.js
+++ src/index.js
@@ -2,28 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Input extends React.Component {
+class Page extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
       value: "",
     };
 
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(value) {
+    this.setState({value: value});
+  }
+
+  render() {
+    return(
+      <Input value={this.state.value} onChange={this.onChange}/>
+    )
+  }
+}
+
+
+class Input extends React.Component {
+  constructor(props) {
+    super(props);
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event) {
-    this.setState({value: event.target.value});
+    this.props.onChange(event.target.value);
   }
 
   render() {
+    const value = this.props.value;
     return (
       <form>
         <label>
           <input
             type="text"
             className="encrypted"
-            value={this.state.value}
+            value={value}
             onChange={this.handleChange}
             placeholder="encrypted string"
           />
@@ -35,6 +55,6 @@ class Input extends React.Component {
 
 
 ReactDOM.render(
-  <Input/>,
+  <Page/>,
   document.getElementById('root')
 );
